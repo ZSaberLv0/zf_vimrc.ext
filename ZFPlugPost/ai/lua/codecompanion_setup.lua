500177a5995dc0c1756177a7ef44b5c02df62bd1
ZSaberLv0 <z@zsaber.com>
2025-03-02 02:19:53 +0800

    update

--- ZFPlugPost/ai/lua/codecompanion_setup.lua
+++ ZFPlugPost/ai/lua/codecompanion_setup.lua
@@ -4,68 +4,70 @@ local addr_chat = vim.g.ZFLLM_API_ADDR_CHAT or '/v1/chat/completions'
 local key = vim.g.ZFLLM_API_KEY or ''
 local model = vim.g.ZFLLM_API_MODEL or 'hunyuan-lite'
 
-require('codecompanion').setup({
-        adapters = {
-            ZFLLM = function()
-                return require('codecompanion.adapters').extend('openai_compatible', {
-                        env = {
-                            url = host,
-                            api_key = key,
-                            chat_url = addr_chat,
-                            models_endpoint = '/v1/models',
-                        },
-                        schema = {
-                            model = {
-                                default = model,
-                            },
-                        },
-                    })
-            end,
-            opts = {
-                show_defaults = false,
-            },
-        },
-        strategies = {
-            chat = {
-                adapter = 'ZFLLM',
-                keymaps = {
-                    send = {modes = {
-                            n = {'<c-s>', '<cr>'},
-                            i = {'<c-s>'},
-                    }},
-                    close = {modes = {
-                            n = {'q'},
-                    }},
-                    stop = {modes = {
-                            n = {'<c-c>'},
-                            i = {'<c-c>'},
-                    }},
-                },
-                roles = {
-                    llm = 'AI :',
-                    user = 'Me :',
-                },
-            },
-            inline = {
-                adapter = 'ZFLLM',
-            },
-        },
-        display = {
-            chat = {
-                window = {
-                    layout = 'buffer',
-                    opts = {
-                        cursorline = true,
+local option = {
+    adapters = {
+        ZFLLM = function()
+            return require('codecompanion.adapters').extend('openai_compatible', {
+                    env = {
+                        url = host,
+                        api_key = key,
+                        chat_url = addr_chat,
+                        models_endpoint = '/v1/models',
                     },
-                },
-                intro_message = '',
-                show_header_separator = true,
-                show_token_count = false,
-                start_in_insert_mode = true,
+                    schema = {
+                        model = {
+                            default = model,
+                        },
+                    },
+                })
+        end,
+        opts = {
+            show_defaults = false,
+        },
+    },
+    strategies = {
+        chat = {
+            adapter = 'ZFLLM',
+            keymaps = {
+                send = {modes = {
+                        n = {'<c-s>', '<cr>'},
+                        i = {'<c-s>'},
+                }},
+                close = {modes = {
+                        n = {'q'},
+                }},
+                stop = {modes = {
+                        n = {'<c-c>'},
+                        i = {'<c-c>'},
+                }},
+            },
+            roles = {
+                llm = 'AI :',
+                user = 'Me :',
             },
         },
-        opts = {
-            send_code = false,
+        inline = {
+            adapter = 'ZFLLM',
         },
-    })
+    },
+    display = {
+        chat = {
+            window = {
+                layout = 'buffer',
+                opts = {
+                    cursorline = true,
+                },
+            },
+            intro_message = '',
+            show_header_separator = true,
+            show_token_count = false,
+            start_in_insert_mode = true,
+        },
+    },
+    opts = {
+        send_code = false,
+    },
+}
+
+require('codecompanion').setup(option)
 
